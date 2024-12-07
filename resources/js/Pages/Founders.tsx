c6b99105f97fe8eee12ea53f1b062fce5b10ce1d
Nick Zou <z.nick00@gmail.com>
2024-12-07 01:27:28 -0500

    add Founders page

---
+++ resources/js/Pages/Founders.tsx
@@ -0,0 +1,10 @@
+export default function Founder() {
+    return (
+        <main className="container mx-auto">
+            <h1>Founders</h1>
+            <ul>
+                <li>Nick Zou</li>
+            </ul>
+        </main>
+    );
+}
