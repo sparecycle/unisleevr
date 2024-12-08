1de5dd1e9626fb5e99798adf53f56590f46285f5
nickzou <z.nick00@gmail.com>
2024-12-08 11:48:17 -0500

    Page/dashboard scaffold (#8)
    * install daisyui
    
    * add some initial actions to logged in state
    
    * add modal for list importing

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,7 +1,10 @@
+import Modal from '@/Components/Modal';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
+import { useState } from 'react';
 
 export default function Dashboard() {
+    const [showModal, setShowModal] = useState(false);
     return (
         <AuthenticatedLayout
             header={
@@ -12,15 +15,37 @@ export default function Dashboard() {
         >
             <Head title="Dashboard" />
 
-            <div className="py-12">
-                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
-                        <div className="p-6 text-gray-900 dark:text-gray-100">
-                            You're logged in!
-                        </div>
+            <div className="container mx-auto px-3 py-4">
+                <div className="-mx-3 flex flex-wrap">
+                    <div className="w-1/2 px-3 pb-3">
+                        <button
+                            className="btn w-full"
+                            onClick={() => setShowModal(true)}
+                        >
+                            Import a list
+                        </button>
+                    </div>
+                    <div className="w-1/2 px-3 pb-3">
+                        <button className="btn w-full">Create a list</button>
+                    </div>
+                    <div className="w-1/2 px-3 pb-3">
+                        <button className="btn w-full">Edit a list</button>
+                    </div>
+                    <div className="w-1/2 px-3 pb-3">
+                        <button className="btn w-full">
+                            View my shared cards
+                        </button>
                     </div>
                 </div>
             </div>
+            <Modal show={showModal} onClose={() => setShowModal(false)}>
+                <div className="w-full p-4">
+                    <h2 className="text-lg font-semibold leading-tight text-gray-800 dark:text-gray-200">
+                        Import a list
+                    </h2>
+                    <input type="file" className="mt-4" name="importFile" />
+                </div>
+            </Modal>
         </AuthenticatedLayout>
     );
 }
