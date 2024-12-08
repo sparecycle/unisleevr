71bb4ceae4ecd4b843f79953978eccc9351bc04f
Nick Zou <z.nick00@gmail.com>
2024-12-07 23:19:52 -0500

    add modal for list importing

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,7 +1,10 @@
+import Modal from '@/Components/Modal';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
+import { useState } from 'react';
 
 export default function Dashboard() {
+    const [showModal, setShowModal] = useState(false);
     return (
         <AuthenticatedLayout
             header={
@@ -15,7 +18,12 @@ export default function Dashboard() {
             <div className="container mx-auto px-3 py-4">
                 <div className="-mx-3 flex flex-wrap">
                     <div className="w-1/2 px-3 pb-3">
-                        <button className="btn w-full">Import a list</button>
+                        <button
+                            className="btn w-full"
+                            onClick={() => setShowModal(true)}
+                        >
+                            Import a list
+                        </button>
                     </div>
                     <div className="w-1/2 px-3 pb-3">
                         <button className="btn w-full">Create a list</button>
@@ -30,6 +38,14 @@ export default function Dashboard() {
                     </div>
                 </div>
             </div>
+            <Modal show={showModal} onClose={() => setShowModal(false)}>
+                <div className="w-full p-4">
+                    <h2 className="text-lg font-semibold leading-tight text-gray-800 dark:text-gray-200">
+                        Import a list
+                    </h2>
+                    <input type="file" className="mt-4" name="importFile" />
+                </div>
+            </Modal>
         </AuthenticatedLayout>
     );
 }
