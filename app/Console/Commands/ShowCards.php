c72e9094546176e3f4417e15ba878cdb4be408cb
jordyarms <jordan@addwonder.com>
2024-12-09 16:56:04 -0500

    adding show cards command for easy checking

--- app/Console/Commands/ShowCards.php
+++ app/Console/Commands/ShowCards.php
@@ -3,28 +3,24 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class ShowCards extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'app:show-cards';
+    protected $signature = 'db:show-cards';
+    protected $description = 'Display cards from the database';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Command description';
-
-    /**
-     * Execute the console command.
-     */
     public function handle()
     {
-        //
+        $cards = DB::table('cards')->take(10)->get();
+
+        $this->info("Displaying the first 10 cards:");
+        $this->table(['ID', 'Name', 'Set'], $cards->map(function ($card) {
+            return [
+                $card->id,
+                $card->name,
+                $card->set,
+            ];
+        })->toArray());
     }
 }
