2ce7114bcc1d51277b6592ec66615edf81c35e16
Nick Zou <z.nick00@gmail.com>
2024-12-13 19:01:36 -0500

    add relationship between card and deck

---
+++ database/migrations/2024_12_13_235406_create_deck_commander_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('deck_commander', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('deck_id')->constrained()->onDelete('cascade');
+            $table->foreignId('card_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('deck_commander');
+    }
+};
