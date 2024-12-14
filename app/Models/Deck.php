d1bcd65f7d9ba7ffc142601a3457568b26220bd4
Nick Zou <z.nick00@gmail.com>
2024-12-13 19:24:20 -0500

    add commanders relationship and set function

--- app/Models/Deck.php
+++ app/Models/Deck.php
@@ -7,4 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 class Deck extends Model
 {
     protected $fillable = ['name']; 
+
+    public function commanders() {
+        return $this->belongsToMany(Card::class, 'deck_commander');
+    }
+    
+    /**
+     * @param array<int> $cardIds
+     */
+    public function setCommanders(array $cardIds) {
+
+        if(count($cardIds)>2) {
+            throw new \Exception('A deck can have at most 2 commanders');
+        }
+
+        if(count($cardIds) === 0) {
+            // we should probably have sound validation to make sure the parners are valid   
+        }
+
+        $this->commanders()->sync($cardIds);
+        
+    }
 }
