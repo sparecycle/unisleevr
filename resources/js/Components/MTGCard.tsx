26097b392cf9ed5e61a2a8c731978afc01b3f609
dazu1990 <adrian.davilazuniga@gmail.com>
2024-12-16 19:01:03 -0500

    fixing dashboard breakpoints

--- resources/js/Components/MTGCard.tsx
+++ resources/js/Components/MTGCard.tsx
@@ -1,98 +1,112 @@
-import { useState } from "react";
-import { ImSpinner11 } from "react-icons/im";
+import { useState } from 'react';
+import { ImSpinner11 } from 'react-icons/im';
 
 type cardDataType = {
-    imgSrc: string,
-    title: string,
-    description: string,
-    backCardData?: cardDataType
-}
+    imgSrc: string;
+    title: string;
+    description: string;
+    backCardData?: cardDataType;
+};
 
-const MTGCard =  ({ imgSrc,title,description,backCardData }:cardDataType) => {
-
+const MTGCard = ({
+    imgSrc,
+    title,
+    description,
+    backCardData,
+}: cardDataType) => {
     const [isFlipped, setIsFlipped] = useState(false);
     const [brokenImage, setBrokenImage] = useState(false);
     const [brokenBackImage, setBrokenBackImage] = useState(false);
 
-
     return (
-        <div className={`card w-full max-md:max-h-[50vw] max-h-[25vw] h-screen  shadow-xl relative rounded-2xl overflow-hidden`}>
-             
-            
-            <div className="card_content relative rounded-2xl" style={{
-                'transformStyle': 'preserve-3d',
-                'transition': 'transform 1s',
-                'transform': isFlipped ? 'rotateY(.5turn)' : 'rotateY(0)'
-            }}>
-               
-                  
-                <div className={`card_front absolute top-0 z-10 w-full h-full`} style={{ 'backfaceVisibility': 'hidden' }}>
+        <div
+            className={`card relative h-screen max-h-[25vw] w-full overflow-hidden rounded-2xl shadow-xl max-md:max-h-[50vw]`}
+        >
+            <div
+                className="card_content relative rounded-2xl"
+                style={{
+                    transformStyle: 'preserve-3d',
+                    transition: 'transform 1s',
+                    transform: isFlipped ? 'rotateY(.5turn)' : 'rotateY(0)',
+                }}
+            >
+                <div
+                    className={`card_front absolute top-0 z-10 h-full w-full`}
+                    style={{ backfaceVisibility: 'hidden' }}
+                >
                     {backCardData && (
-                        <div className="card-actions absolute w-full max-md:max-h-[50vw] max-h-[25vw] h-screen z-10 justify-end content-center">
-                            <button className="btn btn-primary rounded-full bg-stone-50/50 hover:bg-stone-50 hover:rotate-180 border-none" onClick={() => setIsFlipped(!isFlipped)}>
+                        <div className="card-actions absolute z-10 h-screen max-h-[25vw] w-full content-center justify-end max-md:max-h-[50vw]">
+                            <button
+                                className="btn btn-primary rounded-full border-none bg-stone-50/50 hover:rotate-180 hover:bg-stone-50"
+                                onClick={() => setIsFlipped(!isFlipped)}
+                            >
                                 <ImSpinner11 />
                             </button>
                         </div>
                     )}
                     {!brokenImage && (
-
-                        <figure className="rounded-2xl relative">
-                                <img
-                                className="rounded-2xl max-md:max-h-[50vw] max-h-[25vw] h-screen"
+                        <figure className="relative rounded-2xl">
+                            <img
+                                className="h-screen max-h-[25vw] rounded-2xl max-md:max-h-[50vw]"
                                 src={imgSrc}
-                                alt={title} 
-                                onError={()=>{setBrokenImage(true)}}
+                                alt={title}
+                                onError={() => {
+                                    setBrokenImage(true);
+                                }}
                             />
-                            
                         </figure>
                     )}
 
                     <div className="card-body absolute top-0 p-0">
-                        
-                        <div className={`card-body-text w-full ${!brokenImage && 'opacity-0'} bg-slate-800 p-4 rounded-2xl max-md:max-h-[50vw] max-h-[25vw] h-screen`}>
+                        <div
+                            className={`card-body-text w-full ${!brokenImage && 'opacity-0'} h-screen max-h-[25vw] rounded-2xl bg-slate-800 p-4 max-md:max-h-[50vw]`}
+                        >
                             <h2 className="card-title">{title}</h2>
                             <p>{description}</p>
                         </div>
-                       
-                        
                     </div>
-                    
                 </div>
                 {backCardData && (
-                    <div className="card_back absolute top-0 w-full" style={{ 
-                        'backfaceVisibility': 'hidden', 
-                        'transform': 'rotateY(.5turn)'}}>
-                        <div className="card-actions absolute h-full w-full z-10 justify-end content-center">
-                            <button className="btn btn-primary rounded-full bg-stone-50/50 hover:bg-stone-50 hover:rotate-180 border-none" onClick={() => setIsFlipped(!isFlipped)}>
+                    <div
+                        className="card_back absolute top-0 w-full"
+                        style={{
+                            backfaceVisibility: 'hidden',
+                            transform: 'rotateY(.5turn)',
+                        }}
+                    >
+                        <div className="card-actions absolute z-10 h-full w-full content-center justify-end">
+                            <button
+                                className="btn btn-primary rounded-full border-none bg-stone-50/50 hover:rotate-180 hover:bg-stone-50"
+                                onClick={() => setIsFlipped(!isFlipped)}
+                            >
                                 <ImSpinner11 />
                             </button>
                         </div>
-                        <figure className="rounded-2xl relative z-1">
+                        <figure className="z-1 relative rounded-2xl">
                             <img
-                            className="rounded-2xl max-md:max-h-[50vw] max-h-[25vw] h-screen"
-                            src={backCardData.imgSrc}
-                            alt={backCardData.title} 
-                            onError={()=>{setBrokenBackImage(true)}}
-
+                                className="h-screen max-h-[25vw] rounded-2xl max-md:max-h-[50vw]"
+                                src={backCardData.imgSrc}
+                                alt={backCardData.title}
+                                onError={() => {
+                                    setBrokenBackImage(true);
+                                }}
                             />
                         </figure>
                         <div className="card-body absolute top-0 p-0">
-                        
-                            <div className={`card-body-text w-full ${!brokenBackImage && 'opacity-0'} bg-slate-800 p-4 rounded-2xl max-h-[25vw] h-screen`}>
-                                <h2 className="card-title">{backCardData.title}</h2>
+                            <div
+                                className={`card-body-text w-full ${!brokenBackImage && 'opacity-0'} h-screen max-h-[25vw] rounded-2xl bg-slate-800 p-4`}
+                            >
+                                <h2 className="card-title">
+                                    {backCardData.title}
+                                </h2>
                                 <p>{backCardData.description}</p>
                             </div>
-                        
-                            
                         </div>
                     </div>
                 )}
-                
             </div>
-            
         </div>
-    )
+    );
+};
 
-}
-
-export default MTGCard
+export default MTGCard;
--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,9 +1,8 @@
 import Modal from '@/Components/Modal';
+import MTGCard from '@/Components/MTGCard';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 import { useState } from 'react';
-import MTGCard from '@/Components/MTGCard';
-
 
 export default function Dashboard() {
     const [showModal, setShowModal] = useState(false);
@@ -17,7 +16,7 @@ export default function Dashboard() {
         >
             <Head title="Dashboard" />
 
-            <div className="container mx-auto px-3 py-4 ">
+            <div className="container mx-auto px-3 py-4">
                 <div className="-mx-3 flex flex-wrap">
                     <div className="w-1/2 px-3 pb-3">
                         <button
@@ -39,91 +38,92 @@ export default function Dashboard() {
                         </button>
                     </div>
                     {/* this is all dummy content for testing and should be deleted eventually */}
-                    <div className='flex w-full flex-wrap'>
-                        <div className="w-1/4 max-md:w-1/2 px-2 mb-2 text-center">
+                    <div className="flex w-full flex-wrap">
+                        <div className="mb-2 w-full px-2 md:w-1/2 lg:w-1/4">
                             <p>Double sided card</p>
-                            <MTGCard 
-                                imgSrc='https://cards.scryfall.io/large/front/1/8/18a2bdc8-b705-4eb5-b3a5-ff2e2ab8f312.jpg?1626663778'
-                                title='Wandering Archaic'
-                                description='Whenever an opponent casts an instant or sorcery spell, they may pay {2}. If they don’t, you may copy that spell. You may choose new targets for the copy.'
-                                backCardData={
-                                    {
-                                        imgSrc:'https://cards.scryfall.io/large/back/1/8/18a2bdc8-b705-4eb5-b3a5-ff2e2ab8f312.jpg?1626663778',
-                                        title:'Explore the Vastlands',
-                                        description:'Each player looks at the top five cards of their library and may reveal a land card and/or an instant or sorcery card from among them. Each player puts the cards they revealed this way into their hand and the rest on the bottom of their library in a random order. Each player gains 3 life.',
-                                    }
-                                }
+                            <MTGCard
+                                imgSrc="https://cards.scryfall.io/large/front/1/8/18a2bdc8-b705-4eb5-b3a5-ff2e2ab8f312.jpg?1626663778"
+                                title="Wandering Archaic"
+                                description="Whenever an opponent casts an instant or sorcery spell, they may pay {2}. If they don’t, you may copy that spell. You may choose new targets for the copy."
+                                backCardData={{
+                                    imgSrc: 'https://cards.scryfall.io/large/back/1/8/18a2bdc8-b705-4eb5-b3a5-ff2e2ab8f312.jpg?1626663778',
+                                    title: 'Explore the Vastlands',
+                                    description:
+                                        'Each player looks at the top five cards of their library and may reveal a land card and/or an instant or sorcery card from among them. Each player puts the cards they revealed this way into their hand and the rest on the bottom of their library in a random order. Each player gains 3 life.',
+                                }}
                             ></MTGCard>
                         </div>
-                        <div className="w-1/4 max-md:w-1/2 px-2 mb-2">
-                            <p className="text-center">Double sided card 1 bad image</p>
-                            <MTGCard 
-                                imgSrc='https://cards.scryfall.io/large/front/1/8/brokenimage.jpg?1626663778'
-                                title='Wandering Archaic'
-                                description='Whenever an opponent casts an instant or sorcery spell, they may pay {2}. If they don’t, you may copy that spell. You may choose new targets for the copy.'
-                                backCardData={
-                                    {
-                                        imgSrc:'https://cards.scryfall.io/large/back/1/8/18a2bdc8-b705-4eb5-b3a5-ff2e2ab8f312.jpg?1626663778',
-                                        title:'Explore the Vastlands',
-                                        description:'Each player looks at the top five cards of their library and may reveal a land card and/or an instant or sorcery card from among them. Each player puts the cards they revealed this way into their hand and the rest on the bottom of their library in a random order. Each player gains 3 life.',
-                                    }
-                                }
+                        <div className="mb-2 w-full px-2 md:w-1/2 lg:w-1/4">
+                            <p className="text-center">
+                                Double sided card 1 bad image
+                            </p>
+                            <MTGCard
+                                imgSrc="https://cards.scryfall.io/large/front/1/8/brokenimage.jpg?1626663778"
+                                title="Wandering Archaic"
+                                description="Whenever an opponent casts an instant or sorcery spell, they may pay {2}. If they don’t, you may copy that spell. You may choose new targets for the copy."
+                                backCardData={{
+                                    imgSrc: 'https://cards.scryfall.io/large/back/1/8/18a2bdc8-b705-4eb5-b3a5-ff2e2ab8f312.jpg?1626663778',
+                                    title: 'Explore the Vastlands',
+                                    description:
+                                        'Each player looks at the top five cards of their library and may reveal a land card and/or an instant or sorcery card from among them. Each player puts the cards they revealed this way into their hand and the rest on the bottom of their library in a random order. Each player gains 3 life.',
+                                }}
                             ></MTGCard>
                         </div>
-                        <div className="w-1/4 max-md:w-1/2 px-2 mb-2">
-                            <p className="text-center">Double sided card 2 bad images</p>
-                            <MTGCard 
-                                imgSrc='https://cards.scryfall.io/large/front/1/8/brokenimage.jpg?1626663778'
-                                title='Wandering Archaic'
-                                description='Whenever an opponent casts an instant or sorcery spell, they may pay {2}. If they don’t, you may copy that spell. You may choose new targets for the copy.'
-                                backCardData={
-                                    {
-                                        imgSrc:'https://cards.scryfall.io/large/back/1/8/18a2bbroken.jpg?1626663778',
-                                        title:'Explore the Vastlands',
-                                        description:'Each player looks at the top five cards of their library and may reveal a land card and/or an instant or sorcery card from among them. Each player puts the cards they revealed this way into their hand and the rest on the bottom of their library in a random order. Each player gains 3 life.',
-                                    }
-                                }
+                        <div className="mb-2 w-full px-2 md:w-1/2 lg:w-1/4">
+                            <p className="text-center">
+                                Double sided card 2 bad images
+                            </p>
+                            <MTGCard
+                                imgSrc="https://cards.scryfall.io/large/front/1/8/brokenimage.jpg?1626663778"
+                                title="Wandering Archaic"
+                                description="Whenever an opponent casts an instant or sorcery spell, they may pay {2}. If they don’t, you may copy that spell. You may choose new targets for the copy."
+                                backCardData={{
+                                    imgSrc: 'https://cards.scryfall.io/large/back/1/8/18a2bbroken.jpg?1626663778',
+                                    title: 'Explore the Vastlands',
+                                    description:
+                                        'Each player looks at the top five cards of their library and may reveal a land card and/or an instant or sorcery card from among them. Each player puts the cards they revealed this way into their hand and the rest on the bottom of their library in a random order. Each player gains 3 life.',
+                                }}
                             ></MTGCard>
                         </div>
-                        <div className="w-1/4 max-md:w-1/2 px-2 mb-2">
+                        <div className="mb-2 w-full px-2 md:w-1/2 lg:w-1/4">
                             <p className="text-center">standard card</p>
-                            <MTGCard 
-                                imgSrc='https://cards.scryfall.io/large/front/1/4/147bef05-4497-44d5-9dd6-fb5dc08e78f7.jpg?1584830914'
-                                title='Goblin Gathering'
-                                description='Create a number of 1/1 red Goblin creature tokens equal to two plus the number of cards named Goblin Gathering in your graveyard.'
+                            <MTGCard
+                                imgSrc="https://cards.scryfall.io/large/front/1/4/147bef05-4497-44d5-9dd6-fb5dc08e78f7.jpg?1584830914"
+                                title="Goblin Gathering"
+                                description="Create a number of 1/1 red Goblin creature tokens equal to two plus the number of cards named Goblin Gathering in your graveyard."
                             ></MTGCard>
                         </div>
-                        <div className="w-1/4 max-md:w-1/2 px-2 mb-2">
+                        <div className="mb-2 w-full px-2 md:w-1/2 lg:w-1/4">
                             <p className="text-center">card bad image</p>
-                            <MTGCard 
-                                imgSrc='https://cards.scryfall.io/broken image.jpg?1584830914'
-                                title='Goblin Gathering'
-                                description='Create a number of 1/1 red Goblin creature tokens equal to two plus the number of cards named Goblin Gathering in your graveyard.'
+                            <MTGCard
+                                imgSrc="https://cards.scryfall.io/broken image.jpg?1584830914"
+                                title="Goblin Gathering"
+                                description="Create a number of 1/1 red Goblin creature tokens equal to two plus the number of cards named Goblin Gathering in your graveyard."
                             ></MTGCard>
                         </div>
-                        <div className="w-1/4 max-md:w-1/2 px-2 mb-2">
+                        <div className="mb-2 w-full px-2 md:w-1/2 lg:w-1/4">
                             <p className="text-center">standard card</p>
-                            <MTGCard 
-                                imgSrc='https://cards.scryfall.io/large/front/1/4/147bef05-4497-44d5-9dd6-fb5dc08e78f7.jpg?1584830914'
-                                title='Goblin Gathering'
-                                description='Create a number of 1/1 red Goblin creature tokens equal to two plus the number of cards named Goblin Gathering in your graveyard.'
+                            <MTGCard
+                                imgSrc="https://cards.scryfall.io/large/front/1/4/147bef05-4497-44d5-9dd6-fb5dc08e78f7.jpg?1584830914"
+                                title="Goblin Gathering"
+                                description="Create a number of 1/1 red Goblin creature tokens equal to two plus the number of cards named Goblin Gathering in your graveyard."
                             ></MTGCard>
                         </div>
-                        <div className="w-1/4 max-md:w-1/2 px-2 mb-2">
-                            <p className="text-center">standard card to search</p>
-                            <MTGCard 
-                                imgSrc='https://cards.scryfall.io/large/front/6/c/6c44738c-706f-40b2-a09d-b21cd0889049.jpg?1673306670'
-                                title='Combat Research'
-                                description='Enchant creature
+                        <div className="mb-2 w-full px-2 md:w-1/2 lg:w-1/4">
+                            <p className="text-center">
+                                standard card to search
+                            </p>
+                            <MTGCard
+                                imgSrc="https://cards.scryfall.io/large/front/6/c/6c44738c-706f-40b2-a09d-b21cd0889049.jpg?1673306670"
+                                title="Combat Research"
+                                description="Enchant creature
 
                                 Enchanted creature has “Whenever this creature deals combat damage to a player, draw a card.”
                                 
-                                As long as enchanted creature is legendary, it gets +1/+1 and has ward {1}. (Whenever enchanted creature becomes the target of a spell or ability an opponent controls, counter it unless that player pays {1}.)'
+                                As long as enchanted creature is legendary, it gets +1/+1 and has ward {1}. (Whenever enchanted creature becomes the target of a spell or ability an opponent controls, counter it unless that player pays {1}.)"
                             ></MTGCard>
                         </div>
-
                     </div>
-                    
                 </div>
             </div>
             <Modal show={showModal} onClose={() => setShowModal(false)}>
