5f90f7f0eed5448a6bc0144226ddf5d4cba1ac0b
Adrian Davila-Zuniga <adrian.davilazuniga@gmail.com>
2024-12-30 19:15:45 -0500

    Feat: Deck model & deck:new command (#25)
    * instantiating new deck model and deck:new command
    
    * wip user id fkey to deck model
    
    * fixed migration and command for new deck
    
    ---------
    
    Co-authored-by: jordyarms <jordan@addwonder.com>

---
+++ app/Console/Commands/DeckNew.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Deck;
+use Illuminate\Console\Command;
+
+
+class DeckNew extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'deck:new';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Make a new deck for a given user id';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        // $this->info('hello world');
+        $deckName = $this->ask('Deck Name:');
+        $deckUserID = $this->ask('User ID:');
+        $this->info($deckName);
+
+        if ($this->confirm('Is this information correct?')) {
+            $deck = new Deck();
+            $deck->name = $deckName;
+            $deck->user_id = $deckUserID;
+            $deck->save();
+
+            $this->info("Saved.");
+        }
+
+        return 0;
+    }
+}
