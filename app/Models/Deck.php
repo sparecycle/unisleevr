cac45ca1b0e8ee33cdf3775ac636d6745ec1bdb4
Nick Zou <z.nick00@gmail.com>
2025-01-04 12:39:46 -0500

    add cards array to deck

--- app/Models/Deck.php
+++ app/Models/Deck.php
@@ -3,10 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Card;
 
 class Deck extends Model
 {
     protected $fillable = [
-        'name'
+        'name',
+        'cards'
+    ];
+
+    //Temp
+    protected $casts = [
+        'cards' => 'array'
     ];
 }
