b9cb31623994c41a5fcf19e42a6b82b0452d83b3
Adrian Davila-Zuniga <adrian.davilazuniga@gmail.com>
2025-01-18 14:23:40 -0500

    Searchbar (#45)
    * Searchbar component and api calls to scryfall
    
    * searchbar full functionality and pipe to dashbaord
    
    * changed typing and swapped for tailwind
    
    * fixed bugs, types and added backface support, moved parse to FE

--- resources/js/Components/MTGCardFace.tsx
+++ resources/js/Components/MTGCardFace.tsx
@@ -1,7 +1,7 @@
 import { mtgImgSrc } from '../types/mtg';
 
 type cardFaceDataType = {
-    imgSrc: mtgImgSrc;
+    imgUris: mtgImgSrc;
     name: string;
     cardSuperType: string[];
     cardType?: string[];
@@ -9,90 +9,94 @@ type cardFaceDataType = {
     power?: string;
     toughness?: string;
     manaCost?: string[];
-    oracle_text: string;
+    oracleText: string;
     setBrokenImage: (value: boolean) => void;
     brokenImage: boolean;
 };
 
 const MTGCardFace = ({
-    imgSrc,
+    imgUris,
     name,
     cardSuperType,
     cardType,
     power,
     toughness,
     manaCost,
-    oracle_text,
+    oracleText,
     setBrokenImage,
     brokenImage,
-}: cardFaceDataType) => (
-    <>
-        {!brokenImage && (
-            <figure className="relative">
-                <img
-                    className="w-full rounded-[6%]"
-                    src={imgSrc.png}
-                    alt={name}
-                    onError={() => {
-                        setBrokenImage(true);
-                    }}
-                />
-            </figure>
-        )}
+}: cardFaceDataType) => {
+    return (
+        <>
+            {!brokenImage && (
+                <figure className="relative">
+                    <img
+                        className="w-full rounded-[6%]"
+                        src={imgUris.png}
+                        alt={name}
+                        onError={() => {
+                            setBrokenImage(true);
+                        }}
+                    />
+                </figure>
+            )}
 
-        <div className="card-body absolute top-0 w-full p-0">
-            <div
-                className={`card-body-text w-full ${!brokenImage && 'opacity-0'} flex aspect-[2.5/3.5] flex-col justify-between bg-slate-800 p-4`}
-            >
-                <div className="w-full">
-                    <div className="mb-2 flex w-full justify-between align-baseline">
-                        <div>
-                            <h3 className="card-name">{name}</h3>
-                        </div>
-                        <div>
-                            {manaCost && (
-                                <span className="mana-cost">
-                                    {manaCost.map(
-                                        (manaSymbol: string) =>
-                                            ` ${manaSymbol}`,
-                                    )}
-                                </span>
-                            )}
-                        </div>
-                    </div>
+            <div className="card-body absolute top-0 w-full p-0">
+                <div
+                    className={`card-body-text w-full ${!brokenImage && 'opacity-0'} flex aspect-[2.5/3.5] flex-col justify-between bg-slate-800 p-4`}
+                >
                     <div className="w-full">
-                        <p className="mb-2">
-                            {cardSuperType &&
-                                cardSuperType.length > 0 &&
-                                cardSuperType.map(
-                                    (superType: string, index: number) => (
-                                        <span key={index}>{superType} </span>
-                                    ),
+                        <div className="mb-2 flex w-full justify-between align-baseline">
+                            <div>
+                                <h3 className="card-name">{name}</h3>
+                            </div>
+                            <div>
+                                {manaCost && (
+                                    <span className="mana-cost">
+                                        {manaCost.map(
+                                            (manaSymbol: string) =>
+                                                ` ${manaSymbol}`,
+                                        )}
+                                    </span>
                                 )}
-
-                            {cardType &&
-                                cardType.length > 0 &&
-                                `-${cardType.map(
-                                    (cardType: string) => ` ${cardType}`,
-                                )}`}
-                        </p>
-                    </div>
-                </div>
-
-                <div className="flex min-h-[80%] w-full flex-col justify-between">
-                    <div>
-                        <p>{oracle_text}</p>
-                    </div>
-
-                    {(power || toughness) && (
-                        <div className="mt-2 flex w-full justify-end">
-                            <span>{`${power}/${toughness}`}</span>
+                            </div>
                         </div>
-                    )}
+                        <div className="w-full">
+                            <p className="mb-2">
+                                {cardSuperType &&
+                                    cardSuperType.length > 0 &&
+                                    cardSuperType.map(
+                                        (superType: string, index: number) => (
+                                            <span key={index}>
+                                                {superType}{' '}
+                                            </span>
+                                        ),
+                                    )}
+
+                                {cardType &&
+                                    cardType.length > 0 &&
+                                    `-${cardType.map(
+                                        (cardType: string) => ` ${cardType}`,
+                                    )}`}
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="flex min-h-[80%] w-full flex-col justify-between">
+                        <div>
+                            <p>{oracleText}</p>
+                        </div>
+
+                        {(power || toughness) && (
+                            <div className="mt-2 flex w-full justify-end">
+                                <span>{`${power}/${toughness}`}</span>
+                            </div>
+                        )}
+                    </div>
                 </div>
             </div>
-        </div>
-    </>
-);
+        </>
+    );
+};
 
 export default MTGCardFace;
---
+++ resources/js/api/Scryfall.tsx
@@ -0,0 +1,25 @@
+export const scryfallSearch = async (searchQuery: string) => {
+    try {
+        const response = await fetch(
+            `https://api.scryfall.com/cards/search?order=name&q=${searchQuery}`,
+        );
+        const data = await response.json();
+        return data.data;
+    } catch (error) {
+        console.error(error);
+        return error;
+    }
+};
+
+export const scryfallAutoComplete = async (searchQuery: string) => {
+    try {
+        const response = await fetch(
+            `https://api.scryfall.com/cards/autocomplete?q=${searchQuery}`,
+        );
+        const data = await response.json();
+        return data.data;
+    } catch (error) {
+        console.error(error);
+        return error;
+    }
+};
