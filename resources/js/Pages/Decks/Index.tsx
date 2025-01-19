872876ac0a086bc6ec169ce05e5c11b20e0ea248
Nick Zou <z.nick00@gmail.com>
2025-01-18 22:42:25 -0500

    add deck creation form

--- resources/js/Pages/Decks/Index.tsx
+++ resources/js/Pages/Decks/Index.tsx
@@ -1,6 +1,8 @@
 import DeckTile from '@/Components/DeckTile';
 import PageTitle from '@/Components/PageTitle';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { useForm } from '@inertiajs/react';
+import { FormEvent, useState } from 'react';
 
 type Deck = {
     id: number;
@@ -17,10 +19,34 @@ type DecksProps = {
 }
 
 export default function Decks({decks}:DecksProps) {
+    const [isCreating, setIsCreating] = useState(false);
+    const { data, setData, post, processing, reset, errors } = useForm({
+        name: '',
+    });
+
+    const onSubmit = (e:FormEvent) => {
+        e.preventDefault();
+        post(route('decks.store'), { onSuccess: () => reset() });
+    };
     return (
         <AuthenticatedLayout header={<PageTitle>Decks</PageTitle>}>
             <div className="container mx-auto px-3 py-4">
-                <h3 className='text-lg'>Create a new deck</h3>
+                <div>
+                    <h3 className='text-lg'>Create a new deck</h3>
+                    <button onClick={()=> {
+                        setIsCreating(!isCreating)
+                    }} className={'btn bg-slate-80'}
+                    >{isCreating ? 'Cancel' : 'Create'}</button>
+                </div>
+                {isCreating &&
+
+                    <div>
+                        <form onSubmit={onSubmit}>
+                            <input type="text" value={data.name} placeholder="Name Your Deck" className="text-black" onChange={e => setData('name', e.target.value)} />
+                            <button>Create</button>
+                        </form>
+                    </div>
+                }
             </div>
             <div className="container mx-auto px-3 py-4">
                 {decks.data.length > 0 ? (
