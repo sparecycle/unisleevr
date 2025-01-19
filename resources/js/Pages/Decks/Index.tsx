99a39492b643fa61da9f90a380be3584c9708d2c
Nick Zou <z.nick00@gmail.com>
2025-01-19 17:39:25 -0500

    remove daisyui

--- resources/js/Pages/Decks/Index.tsx
+++ resources/js/Pages/Decks/Index.tsx
@@ -32,17 +32,16 @@ export default function Decks({decks}:DecksProps) {
         <AuthenticatedLayout header={<PageTitle>Decks</PageTitle>}>
             <div className="container mx-auto px-3 py-4">
                 <div>
-                    <h3 className='text-lg'>Create a new deck</h3>
-                    <button onClick={()=> {
+                {!isCreating && <button onClick={()=> {
                         setIsCreating(!isCreating)
-                    }} className={'btn bg-slate-80'}
-                    >{isCreating ? 'Cancel' : 'Create'}</button>
+                    }} className={'btn bg-lg border border-solid rounded-md px-3 py-2 border-slate-600'}
+                    >Create a deck</button>}
                 </div>
                 {isCreating &&
 
                     <div>
                         <form onSubmit={onSubmit}>
-                            <input type="text" value={data.name} placeholder="Name Your Deck" className="text-black" onChange={e => setData('name', e.target.value)} />
+                            <input type="text" value={data.name} placeholder="Name Your Deck" className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 ps-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500" onChange={e => setData('name', e.target.value)} />
                             <button>Create</button>
                         </form>
                     </div>
@@ -51,7 +50,7 @@ export default function Decks({decks}:DecksProps) {
             <div className="container mx-auto px-3 py-4">
                 {decks.data.length > 0 ? (
                     <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-                        {decks.data.map(deck => <DeckTile title={deck.name} /> )}
+                        {decks.data.map((deck, idx) => <DeckTile key={idx} title={deck.name} /> )}
                     </div>
                 ) : (
                     <div>No decks found.</div>
