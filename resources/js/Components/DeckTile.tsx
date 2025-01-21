c6262a4dee50c9670658bc6b43d6db564250b51c
Nick Zou <z.nick00@gmail.com>
2025-01-20 21:47:38 -0500

    add details and edit buttons

--- resources/js/Components/DeckTile.tsx
+++ resources/js/Components/DeckTile.tsx
@@ -3,11 +3,15 @@ type DeckTileProps = {
 };
 const DeckTile = ({title}:DeckTileProps) => {
     return (
-        <div className="card">
-            <div className="card-body">
-                <h4 className="card-title">
+        <div className="border flex flex-col rounded-md border-solid border-slate-600">
+            <div className="p-4  gap-4">
+                <h4 className="text-md">
                     {title}
                 </h4>
+            </div>
+            <div className="flex">
+                <button className="w-1/2 flex border-t border-solid border-slate-600 justify-center items-center p-4 border-r">details</button>
+                <button className="w-1/2 border-t border-solid border-slate-600 justify-center items-center flex p-4">edit</button>
             </div>
         </div>
     )
