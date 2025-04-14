04f0e5f6a14b8368017114a24111988df3b397cc
Nick Zou <z.nick00@gmail.com>
2025-04-13 21:46:40 -0400

    add parentSetter

--- resources/js/utilities/updateDecks.ts
+++ resources/js/utilities/updateDecks.ts
@@ -1,7 +1,8 @@
 import { CardDataType, Deck } from '@/types/mtg';
 import { router } from '@inertiajs/react';
+import { Dispatch, SetStateAction } from 'react';
 
-const updateDecks = (decks: Deck[], user_id: number, card: CardDataType) => {
+const updateDecks = (decks: Deck[], user_id: number, card: CardDataType, parentSetter?: Dispatch<SetStateAction<boolean>>) => {
     const updatedDecks = decks.map((deck) => ({
         id: deck.id,
         name: deck.name,
@@ -15,9 +16,15 @@ const updateDecks = (decks: Deck[], user_id: number, card: CardDataType) => {
             only: [],
             onSuccess: () => {
                 console.log(`Card added to decks`);
+                if(parentSetter) {
+                    parentSetter(true);
+                }
             },
             onError: (errors) => {
                 console.error(`Error update decks`, errors);
+                if(parentSetter) {
+                    parentSetter(false);
+                }
             },
         },
     );
