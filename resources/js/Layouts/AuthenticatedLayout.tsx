eabf9c17f09be0a7b0dd5a50765f8023adba52cb
Nick Zou <z.nick00@gmail.com>
2025-04-25 16:25:03 -0400

    update page header with container

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -180,7 +180,7 @@ export default function Authenticated({
 
             {header && (
                 <header className="bg-white shadow dark:bg-zinc-800">
-                    <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                    <div className="mx-auto container px-4 py-6 sm:px-6 lg:px-8">
                         {header}
                     </div>
                 </header>
