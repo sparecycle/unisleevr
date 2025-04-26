b7d97eb9c4db314a005dde2d22ce67a821aa4ed8
Nick Zou <z.nick00@gmail.com>
2025-04-26 14:05:04 -0400

    add remove capability from cards modal

--- resources/js/Components/RemoveCardModalContent.tsx
+++ resources/js/Components/RemoveCardModalContent.tsx
@@ -1,5 +1,8 @@
-import { CardsWithDecks } from '@/types/mtg';
-import { Dispatch, SetStateAction } from 'react';
+import { CardsWithDecks, Deck } from '@/types/mtg';
+import updateDecks from '@/utilities/updateDecks';
+import { usePage } from '@inertiajs/react';
+import { Dispatch, SetStateAction, useState, useEffect } from 'react';
+import Button from './Button';
 import LabeledCheckbox from './LabeledCheckbox';
 
 type Props = {
@@ -8,13 +11,56 @@ type Props = {
 };
 
 const RemoveCardModalContent = ({ card, modalClose }: Props) => {
+    const { auth } = usePage().props;
+    const [submitted, setSubmitted] = useState<boolean>(false);
+    const [submitting, setSubmitting] = useState<boolean>(false);
+    const [selectedDecks, setSelectedDecks] = useState<Deck[] | []>(
+        card.decks as Deck[],
+    );
+    const handleCheckbox = (deck: Deck) => {
+        const cardInDeck = deck.cards.filter(d=>d.id === card.id).length > 0;
+        if(cardInDeck) {
+          setSelectedDecks(selectedDecks.filter(d=>d.id !== deck.id));
+        } else {
+            setSelectedDecks([...selectedDecks, deck]);
+        }
+    };
+    useEffect(() => {
+        if (submitted === true) {
+            if (modalClose !== undefined) {
+                modalClose(false);
+            }
+        }
+    }, [submitted]);
+    const onSubmit = async () => {
+        setSubmitting(true);
+        try {
+            updateDecks(
+                selectedDecks,
+                auth.user.id,
+                card as CardsWithDecks,
+                setSubmitted,
+                'remove',
+            );
+        } catch (error) {
+            console.error('One of the promises failed:', error);
+        }
+    };
     return (
         <div className="flex flex-col gap-2">
             <div>{card.name}</div>
             <div>
                 {card.decks?.map((deck) => (
-                    <LabeledCheckbox initialState={true} key={deck.id} label={deck.name} />
+                    <LabeledCheckbox
+                        initialState={true}
+                        key={deck.id}
+                        label={deck.name}
+                        parentSetter={() => handleCheckbox(deck)}
+                    />
                 ))}
+            </div>
+            <div className="flex items-center justify-center">
+                <Button onClick={onSubmit}>Remove Card</Button>
             </div>
         </div>
     );
