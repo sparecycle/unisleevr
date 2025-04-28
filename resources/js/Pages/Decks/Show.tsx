1ffcffbbdc1a31472feb6f78209b221f39dc8b01
Nick Zou <z.nick00@gmail.com>
2025-04-27 23:54:49 -0400

    add remove to deck view

--- resources/js/Pages/Decks/Show.tsx
+++ resources/js/Pages/Decks/Show.tsx
@@ -2,17 +2,27 @@ import CardList from '@/Components/CardList';
 import PageTitle from '@/Components/PageTitle';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Deck as DeckProps } from '@/types/deck';
+import { CardDataType } from '@/types/mtg';
+import updateDecks from '@/utilities/updateDecks';
+import { usePage } from '@inertiajs/react';
+import { useState } from 'react';
 
 type Props = {
     deck: DeckProps;
 };
 
-export default function Deck({ deck: { name, cards } }: Props) {
+export default function Deck({ deck }: Props) {
+    const user = usePage().props.auth.user;
+    const [submitting, setSubmitting] = useState(false);
+    const handleDelete = (card:CardDataType) => {
+        deck.cards = deck.cards.filter(c=>c.id !== card.name);
+        updateDecks([deck], user.id, setSubmitting);
+    }
     return (
-        <AuthenticatedLayout header={<PageTitle>{name}</PageTitle>}>
+        <AuthenticatedLayout header={<PageTitle>{deck.name}</PageTitle>}>
             <div className="container mx-auto px-3 py-4">
                 <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
-                    <CardList cards={cards} />
+                    <CardList cards={deck.cards} parentDelete={handleDelete} />
                 </div>
             </div>
         </AuthenticatedLayout>
