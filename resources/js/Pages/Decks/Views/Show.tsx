c57334f5d02b31d1a4696f25e3f141b017b94e00
Nick Zou <z.nick00@gmail.com>
2025-05-07 20:13:30 -0400

    update Views/Show.tsx

---
+++ resources/js/Pages/Decks/Views/Show.tsx
@@ -0,0 +1,34 @@
+import CardList from '@/Components/CardList';
+import { CardDataType, CardsWithDecks, Deck } from '@/types/mtg';
+import updateDecks from '@/utilities/updateDecks';
+import { usePage } from '@inertiajs/react';
+
+type Props = {
+    deck: Deck;
+};
+
+const Show = ({ deck }: Props) => {
+    const user = usePage().props.auth.user;
+    const handleCardDelete = (card: CardsWithDecks | CardDataType) => {
+        console.log(deck);
+        updateDecks([deck], user.id, card, () => {}, 'remove');
+    };
+    return (
+        <div className="container mx-auto px-3 py-4">
+            <h2 className="text-lg font-semibold">Commanders</h2>
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+                <CardList cards={deck.commanders} />
+            </div>
+            <h2 className="text-lg font-semibold">Cards</h2>
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+                <CardList
+                    cards={deck.cards}
+                    showDecks={false}
+                    parentDelete={handleCardDelete}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default Show;
