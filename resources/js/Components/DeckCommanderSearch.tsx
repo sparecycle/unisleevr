7cbada4aec99e9b44fa9fcc86174de00cc60edaf
Nick Zou <z.nick00@gmail.com>
2025-05-09 22:25:15 -0400

    create separate DeckCommanderSearch component
    -create DeckCommanderSearch component
    -remove commander specific logic checks
    -name invalid an optional prop of NametagButton, set default value as
    false

---
+++ resources/js/Components/DeckCommanderSearch.tsx
@@ -0,0 +1,71 @@
+
+import { CardDataType, mtgColorStrings } from '@/types/mtg';
+import { useCallback } from 'react';
+import NametagButton from './NametagButton';
+import Searchbar from './Searchbar';
+
+type Props = {
+    isSearching: boolean;
+    parentSetter: (value: CardDataType[] | []) => void;
+    cards: CardDataType[];
+    processing: boolean;
+    removeAction: (card: CardDataType) => void;
+    commanderColorIdentity: mtgColorStrings[];
+};
+
+type CardDataTypeWithInvalidColor = CardDataType & {
+    isInvalidColor: boolean;
+};
+
+const DeckCommanderSearch = ({
+    isSearching,
+    parentSetter,
+    cards,
+    processing,
+    removeAction,
+    commanderColorIdentity,
+}: Props) => {
+    return (
+        <>
+            {isSearching && (
+                <Searchbar
+                    autofocus={false}
+                    parentSetter={parentSetter}
+                    specificCard
+                    CTAText={
+                        'Add Commander'                   }
+                    placeholderText={
+                        'Add commanders to your deck'
+                    }
+                    cardsToExclude={cards}
+                ></Searchbar>
+            )}
+
+            {cards.length > 0 && (
+                <>
+                    <ul className="flex max-h-[30vh] w-full flex-wrap overflow-y-auto rounded-md border border-solid border-zinc-800 bg-zinc-900 p-2">
+                        {cards.map((card) => {
+                            return (
+                                <li
+                                    key={`selectedcard-${card.id}`}
+                                    className="m-1"
+                                >
+                                    <NametagButton
+                                        aria-label={`remove ${card.name} from deck`}
+                                        disabled={processing || !isSearching}
+                                        onClick={() => removeAction(card)}
+                                        showClose={processing || isSearching}
+                                    >
+                                        {card.name}
+                                    </NametagButton>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </>
+            )}
+        </>
+    );
+};
+
+export default DeckCommanderSearch;
