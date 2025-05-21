08afd9f74bf566b67b4c1df5f1328a0c7866a19f
Nick Zou <z.nick00@gmail.com>
2025-05-20 23:02:22 -0400

    add hamburger button

--- resources/js/Pages/Welcome/Header.tsx
+++ resources/js/Pages/Welcome/Header.tsx
@@ -1,7 +1,14 @@
+import Hamburger from 'hamburger-react';
+import { useState } from 'react';
+
 const Header = () => {
+    const [open, setOpen] = useState<boolean>(false);
     return (
         <header className="sticky m-3 flex h-12 items-center justify-between rounded-md bg-black p-4 text-white">
-            header
+            <div>header</div>
+            <div className="flex h-8 w-8 items-center justify-center">
+                <Hamburger size={20} toggled={open} toggle={setOpen} />
+            </div>
         </header>
     );
 };
