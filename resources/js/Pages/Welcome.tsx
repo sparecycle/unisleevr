b2533144e8e5fed948ac4729155b818550675ef9
Nick Zou <z.nick00@gmail.com>
2025-05-20 22:42:02 -0400

    add Header component

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -1,17 +1,17 @@
 import { PageProps } from '@/types';
 import { Head } from '@inertiajs/react';
+import Header from './Welcome/Header';
 
 export default function Welcome({
     auth,
     laravelVersion,
     phpVersion,
 }: PageProps<{ laravelVersion: string; phpVersion: string }>) {
-
     return (
         <>
             <Head title="Welcome" />
-            <div className="relative min-h-screen flex flex-col bg-zinc-900">
-                <header className="text-white sticky flex justify-between items-center m-3 rounded-md h-12 p-4 bg-black">header</header>
+            <div className="relative flex min-h-screen flex-col bg-zinc-900">
+                <Header />
                 <main className=""></main>
 
                 <footer className="py-16 text-center text-sm text-black dark:text-white/70">
---
+++ resources/js/Pages/Welcome/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="sticky m-3 flex h-12 items-center justify-between rounded-md bg-black p-4 text-white">
+            header
+        </header>
+    );
+};
+
+export default Header;
