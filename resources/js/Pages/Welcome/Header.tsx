424d119b2ef51a79ab72f468743132187a279eeb
Nick Zou <z.nick00@gmail.com>
2025-05-21 21:50:37 -0400

    add mobile menu

--- resources/js/Pages/Welcome/Header.tsx
+++ resources/js/Pages/Welcome/Header.tsx
@@ -1,15 +1,25 @@
-import Hamburger from 'hamburger-react';
+import {Slant as Hamburger} from 'hamburger-react';
 import { useState } from 'react';
 
 const Header = () => {
     const [open, setOpen] = useState<boolean>(false);
     return (
-        <header className="sticky m-3 flex h-12 items-center justify-between rounded-md bg-black p-4 text-white">
-            <div>header</div>
-            <div className="flex h-8 w-8 items-center justify-center">
-                <Hamburger size={20} toggled={open} toggle={setOpen} />
+        <>
+            <header className="sticky m-3 flex h-12 items-center justify-between rounded-md bg-black p-4 text-white">
+                <div>logo</div>
+                <div className="flex h-8 w-8 items-center justify-center relative">
+                    <Hamburger size={12} toggled={open} toggle={setOpen} />
+                </div>
+            </header>
+            <div className="absolute left-0 top-[72px] mx-3 mb-3 h-[calc(100vh-84px)] w-[calc(100%-24px)] rounded-md bg-black p-4">
+                <nav>
+                    <ul>
+                        <li className="text-white">menu item one</li>
+                        <li className="text-white">menu item one</li>
+                    </ul>
+                </nav>
             </div>
-        </header>
+        </>
     );
 };
 
