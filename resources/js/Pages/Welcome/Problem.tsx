dc716ec48872b67388ca12423bb469cafa2fd0ae
Nick Zou <z.nick00@gmail.com>
2025-06-01 17:58:05 -0400

    add copy to Problem section

--- resources/js/Pages/Welcome/Problem.tsx
+++ resources/js/Pages/Welcome/Problem.tsx
@@ -1,5 +1,15 @@
 const Problem = () => {
-    return <section>problem section</section>;
+    return (
+        <section>
+            <h2>Where's My Rhystic Study?</h2>
+            <p>Many decks. One expensive card. Endless compromises.</p>
+            <ul>
+                <li>Sick of keeping track of which deck has that card?</li>
+                <li>Tired of buying the same card over and over again?</li>
+                <li>Are your proxies out of control?</li>
+            </ul>
+        </section>
+    );
 };
 
 export default Problem;
