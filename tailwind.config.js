cf7ec6df1552c2b2721ba503fda17ed7b7f96106
Nick Zou <z.nick00@gmail.com>
2025-06-01 21:30:33 -0400

    add Genos font

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,7 @@ export default {
             },
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                title: ['Genos', ...defaultTheme.fontFamily.sans],
             },
         },
     },
