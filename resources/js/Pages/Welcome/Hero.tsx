94fcfe9f656d7e5307d79819ea1bf959f9337b85
Nick Zou <z.nick00@gmail.com>
2025-06-01 21:22:44 -0400

    update hero styles with new font

--- resources/js/Pages/Welcome/Hero.tsx
+++ resources/js/Pages/Welcome/Hero.tsx
@@ -1,13 +1,21 @@
 const Hero = () => {
     return (
         <section className="h-[75vh]">
-            <div className="mx-auto flex h-full w-9/12 flex-col items-center justify-center text-center text-white">
-                <h1 className="text-3xl">One Card, Many Decks</h1>
-                <p>
-                    Manage your Commander staples across every deck with a
-                    single collection.
-                </p>
-                <button>Join the Waitlist</button>
+            <div className="container mx-auto flex h-full flex-col items-center justify-center gap-3 px-4 text-center text-white">
+                <h1 className="font-title text-3xl font-semibold uppercase leading-10">
+                    One Card, Many Decks
+                </h1>
+                <div className="w-5/6">
+                    <p className="text-base leading-6">
+                        Manage your Commander staples across every deck with a
+                        single collection.
+                    </p>
+                </div>
+                <div className="py-2">
+                    <button className="rounded-md bg-rose-700 px-6 py-2 font-title text-lg font-medium uppercase">
+                        Join the Waitlist
+                    </button>
+                </div>
             </div>
         </section>
     );
