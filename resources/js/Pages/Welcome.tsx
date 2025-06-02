6ceb1c4cb06f9e3e0f7318892780c6def26956bd
Nick Zou <z.nick00@gmail.com>
2025-06-01 21:07:26 -0400

    add font

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -11,7 +11,19 @@ export default function Welcome({
 }: PageProps<{ laravelVersion: string; phpVersion: string }>) {
     return (
         <>
-            <Head title="Welcome" />
+            <Head>
+                <title>Welcome</title>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link
+                    rel="preconnect"
+                    href="https://fonts.gstatic.com"
+                    crossOrigin="anonymous"
+                />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Genos:ital,wght@0,100..900;1,100..900&family=Oxanium:wght@200..800&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+                    rel="stylesheet"
+                />
+            </Head>
             <div className="relative flex min-h-screen w-full flex-col overflow-x-clip bg-zinc-900">
                 <Header auth={auth} />
                 <main>
