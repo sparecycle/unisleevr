53df375453784def634b71d6ffa8d0cd07715cf4
Nick Zou <z.nick00@gmail.com>
2025-06-01 21:42:56 -0400

    move container into Section component

--- resources/js/Pages/Welcome/Problem.tsx
+++ resources/js/Pages/Welcome/Problem.tsx
@@ -1,14 +1,17 @@
+import Section from '@/Components/Unauthenticated/Section';
+import SectionTitle from '@/Components/Unauthenticated/SectionTitle';
+
 const Problem = () => {
     return (
-        <section>
-            <h2>Where's My Rhystic Study?</h2>
+        <Section className="min-h-1/2 bg-zinc-700">
+            <SectionTitle>Where's My Rhystic Study?</SectionTitle>
             <p>Many decks. One expensive card. Endless compromises.</p>
-            <ul>
+            <ul className="my-3 list-disc pl-5">
                 <li>Sick of keeping track of which deck has that card?</li>
                 <li>Tired of buying the same card over and over again?</li>
                 <li>Are your proxies out of control?</li>
             </ul>
-        </section>
+        </Section>
     );
 };
 
