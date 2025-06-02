99c9bd09be782e02b3f955255d592327e6d101a2
Nick Zou <z.nick00@gmail.com>
2025-06-01 21:44:37 -0400

    add className to Section component

--- resources/js/Components/Unauthenticated/Section.tsx
+++ resources/js/Components/Unauthenticated/Section.tsx
@@ -7,7 +7,11 @@ type Props = {
 
 const Section = ({ className, children }: Props) => {
     return (
-        <section className={`py-8 text-white ${className}`}>{children}</section>
+        <section className={`py-14 text-white ${className}`}>
+            <div className="container mx-auto flex flex-col gap-3 px-4">
+                {children}
+            </div>
+        </section>
     );
 };
 
