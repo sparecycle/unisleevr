a7d222baca912ddc390bc0acd8d7c73611ada838
Nick Zou <z.nick00@gmail.com>
2025-06-01 22:20:46 -0400

    add IconText component

---
+++ resources/js/Components/Unauthenticated/IconText.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from 'react';
+
+type Props = {
+    icon?: ReactNode;
+    text: string;
+};
+const IconText = ({ icon, text }: Props) => {
+    return (
+        <div className="flex flex-col gap-2 text-center text-sm">
+            {icon && icon}
+            {text}
+        </div>
+    );
+};
+
+export default IconText;
