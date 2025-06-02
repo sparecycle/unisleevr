624d9646c159370e21afec81d20f517f0e655670
Nick Zou <z.nick00@gmail.com>
2025-06-01 21:27:30 -0400

    use HeroTitle component

--- resources/js/Pages/Welcome/Hero.tsx
+++ resources/js/Pages/Welcome/Hero.tsx
@@ -1,10 +1,9 @@
+import Title from '@/Components/Unauthenticated/HeroTitle';
 const Hero = () => {
     return (
         <section className="h-[75vh]">
             <div className="container mx-auto flex h-full flex-col items-center justify-center gap-3 px-4 text-center text-white">
-                <h1 className="font-title text-3xl font-semibold uppercase leading-10">
-                    One Card, Many Decks
-                </h1>
+                <Title>One Card, Many Decks</Title>
                 <div className="w-5/6">
                     <p className="text-base leading-6">
                         Manage your Commander staples across every deck with a
