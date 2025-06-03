54be6b5c8aa9956fbad232dfcac195cb9412ed03
Nick Zou <z.nick00@gmail.com>
2025-06-02 23:13:45 -0400

    add overflow-hidden to entire hero section

--- resources/js/Pages/Welcome/Hero.tsx
+++ resources/js/Pages/Welcome/Hero.tsx
@@ -2,8 +2,8 @@ import Card from '@/Components/Unauthenticated/HeroCard';
 import Title from '@/Components/Unauthenticated/HeroTitle';
 const Hero = () => {
     return (
-        <section className="relative h-[75vh]">
-            <div className="container relative z-2 mx-auto flex h-full flex-col items-center justify-center gap-3 px-4 text-center text-white">
+        <section className="relative h-[75vh] overflow-hidden">
+            <div className="relative z-2 container mx-auto flex h-full flex-col items-center justify-center gap-3 px-4 text-center text-white">
                 <Title>One Card, Many Decks</Title>
                 <div className="w-5/6">
                     <p className="text-base leading-6">
@@ -12,7 +12,7 @@ const Hero = () => {
                     </p>
                 </div>
                 <div className="py-2">
-                    <button className="rounded-md bg-rose-700 px-6 py-2 font-title text-lg font-medium uppercase">
+                    <button className="font-title rounded-md bg-rose-700 px-6 py-2 text-lg font-medium uppercase">
                         Join the Waitlist
                     </button>
                 </div>
