838bee666a37083f65c3f437ad9a9b1ab4032878
Nick Zou <z.nick00@gmail.com>
2025-06-04 13:55:52 -0400

    add card highlight animation

--- resources/js/Components/Unauthenticated/HeroCard.tsx
+++ resources/js/Components/Unauthenticated/HeroCard.tsx
@@ -3,7 +3,7 @@ const HeroCard = () => {
         <div className="absolute -bottom-14 left-1/2 z-0 w-2/3 -translate-x-1/2">
             <div className="z-0 perspective-near transform-3d">
                 <div className="rotate-x-42">
-                    <div className="rounded-mtg overflow-hidden text-white">
+                    <div className="hero-card-wrapper rounded-mtg relative overflow-hidden text-white">
                         <img src="https://cards.scryfall.io/large/front/c/9/c94079dd-023a-41b2-9004-95bbb0e41267.jpg?1593814137" />
                     </div>
                 </div>
