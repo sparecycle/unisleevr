cfd745053a52aa9cd1b6c213556510664bd90da4
Nick Zou <z.nick00@gmail.com>
2025-06-04 14:18:45 -0400

    add eyebrow to SectionTitle component

--- resources/js/Components/Unauthenticated/SectionTitle.tsx
+++ resources/js/Components/Unauthenticated/SectionTitle.tsx
@@ -3,14 +3,22 @@ import { ReactNode } from 'react';
 type Props = {
     children: ReactNode;
     className?: string;
+    eyebrow?: string;
 };
-const SectionTitle = ({ children, className }: Props) => {
+const SectionTitle = ({ children, className, eyebrow }: Props) => {
     return (
-        <h2
-            className={`font-title text-2xl font-semibold uppercase ${className}`}
-        >
-            {children}
-        </h2>
+        <hgroup>
+            {eyebrow && (
+                <div className="font-title border-l-2 border-solid border-l-rose-600 pl-2 text-base leading-tight font-medium text-white">
+                    {eyebrow}
+                </div>
+            )}
+            <h2
+                className={`font-title text-[1.6rem] font-semibold uppercase ${className}`}
+            >
+                {children}
+            </h2>
+        </hgroup>
     );
 };
 
