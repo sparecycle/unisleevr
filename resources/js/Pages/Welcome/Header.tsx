2af388f02a90b01b34b045cab0b581fa916f3180
Nick Zou <z.nick00@gmail.com>
2025-06-04 14:30:31 -0400

    add z-index for menu

--- resources/js/Pages/Welcome/Header.tsx
+++ resources/js/Pages/Welcome/Header.tsx
@@ -10,14 +10,14 @@ const Header = ({ auth }: Props) => {
     const [open, setOpen] = useState<boolean>(false);
     return (
         <>
-            <header className="sticky m-3 flex h-12 items-center justify-between rounded-md bg-black text-white">
+            <header className="sticky z-10 m-3 flex h-12 items-center justify-between rounded-md bg-black text-white">
                 <div className="grow p-4">logo</div>
                 <div className="relative h-12 w-12 grow-0">
                     <Hamburger size={20} toggled={open} toggle={setOpen} />
                 </div>
             </header>
             <div
-                className={`absolute top-[72px] mx-3 mb-3 h-[calc(100vh-84px)] w-[calc(100%-24px)] rounded-md bg-black p-4 ${open ? 'left-0' : 'left-full'}`}
+                className={`absolute top-[72px] z-10 mx-3 mb-3 h-[calc(100vh-84px)] w-[calc(100%-24px)] rounded-md bg-black p-4 ${open ? 'left-0' : 'left-full'}`}
             >
                 <nav>
                     <ul>
