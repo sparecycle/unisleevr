534c1c5e31ceb0e75d748b5b02df54910ed4109a
Nick Zou <z.nick00@gmail.com>
2025-06-04 20:20:12 -0400

    update SectionTitle with tablet sizes

--- resources/js/Components/Unauthenticated/SectionTitle.tsx
+++ resources/js/Components/Unauthenticated/SectionTitle.tsx
@@ -9,12 +9,12 @@ const SectionTitle = ({ children, className, eyebrow }: Props) => {
     return (
         <hgroup>
             {eyebrow && (
-                <div className="font-title border-l-2 border-solid border-l-rose-600 pl-2 text-base leading-tight font-medium text-white">
+                <div className="font-title border-l-2 border-solid border-l-rose-600 pl-2 text-base leading-tight font-medium text-white md:text-xl">
                     {eyebrow}
                 </div>
             )}
             <h2
-                className={`font-title text-[1.65rem] font-semibold uppercase ${className}`}
+                className={`font-title text-[1.65rem] font-semibold uppercase md:text-3xl ${className ? className : ''}`}
             >
                 {children}
             </h2>
