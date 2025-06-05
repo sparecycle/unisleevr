d904f40e2546e3ea644afeda4cafe8b3679d9010
Nick Zou <z.nick00@gmail.com>
2025-06-04 22:53:17 -0400

    add Icons to Guilds

---
+++ resources/js/Components/Icons/IconGuilds.tsx
@@ -0,0 +1,29 @@
+import { ReactNode } from 'react';
+import IconForest from './IconForest';
+import IconIsland from './IconIsland';
+import IconMountain from './IconMountain';
+
+type PipWrapperProps = {
+    children: ReactNode;
+};
+const PipWrapper = ({ children }: PipWrapperProps) => {
+    return <div className="aspect-square w-1/3">{children}</div>;
+};
+
+const IconGuilds = () => {
+    return (
+        <div className="relative aspect-square">
+            <PipWrapper>
+                <IconIsland />
+            </PipWrapper>
+            <PipWrapper>
+                <IconMountain />
+            </PipWrapper>
+            <PipWrapper>
+                <IconForest />
+            </PipWrapper>
+        </div>
+    );
+};
+
+export default IconGuilds;
