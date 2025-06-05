1b86fa2f10402712efd9f81eabc691f06a22e414
Nick Zou <z.nick00@gmail.com>
2025-06-04 21:17:40 -0400

    add classes to constrain inner svg

--- resources/js/Components/Unauthenticated/IconText.tsx
+++ resources/js/Components/Unauthenticated/IconText.tsx
@@ -7,7 +7,15 @@ type Props = {
 const IconText = ({ icon, text }: Props) => {
     return (
         <div className="flex flex-col items-center gap-2 text-center text-sm">
-            {icon && <div className={'w-12 [&_path]:fill-white'}>{icon}</div>}
+            {icon && (
+                <div
+                    className={
+                        'flex h-12 w-12 items-center justify-center [&_path]:fill-white [&_svg]:max-h-full [&_svg]:max-w-full'
+                    }
+                >
+                    {icon}
+                </div>
+            )}
             <p className="text-base">{text}</p>
         </div>
     );
