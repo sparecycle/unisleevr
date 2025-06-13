ea33b569baafb70b027ef3f982a8a09113e1b4e0
Nick Zou <z.nick00@gmail.com>
2025-06-13 14:34:54 -0400

    update Problem copy

--- resources/js/Pages/Welcome/Problem.tsx
+++ resources/js/Pages/Welcome/Problem.tsx
@@ -1,4 +1,3 @@
-import ListItem from '@/Components/Unauthenticated/ListItem';
 import Paragraph from '@/Components/Unauthenticated/Paragraph';
 import Section from '@/Components/Unauthenticated/Section';
 import SectionTitle from '@/Components/Unauthenticated/SectionTitle';
@@ -6,21 +5,15 @@ import SectionTitle from '@/Components/Unauthenticated/SectionTitle';
 const Problem = () => {
     return (
         <Section className="min-h-1/2 bg-zinc-700">
-            <SectionTitle eyebrow="Buying duplicates is expensive">
+            <SectionTitle eyebrow="Never buy duplicates again.">
                 Where's My Rhystic Study?
             </SectionTitle>
             <Paragraph>
-                Many decks. One expensive card. Endless compromises.
+                Keeping track of shared staples in Commander is a headache.
+                You’re never sure which deck your Rhystic Study is in, you’ve
+                bought extras just to stay legal, and proxies are piling up
+                fast. It’s chaotic, expensive, and all too common.
             </Paragraph>
-            <ul className="my-3 list-disc pl-5">
-                <ListItem>
-                    Sick of keeping track of which deck has that card?
-                </ListItem>
-                <ListItem>
-                    Tired of buying the same card over and over again?
-                </ListItem>
-                <ListItem>Are your proxies out of control?</ListItem>
-            </ul>
         </Section>
     );
 };
