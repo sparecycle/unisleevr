86987a1201f3fd22f5ce604507627d1ca5aca057
Nick Zou <z.nick00@gmail.com>
2025-06-13 16:43:22 -0400

    change alignment on UseCase layout

--- resources/js/Components/Unauthenticated/TitleText.tsx
+++ resources/js/Components/Unauthenticated/TitleText.tsx
@@ -1,10 +1,13 @@
 type Props = {
     title: string;
     text: string;
+    className?: string;
 };
-const TitleText = ({ title, text }: Props) => {
+const TitleText = ({ title, text, className }: Props) => {
     return (
-        <div className="flex flex-col items-center justify-start gap-y-1.5 p-3 text-center">
+        <div
+            className={`flex flex-col items-center justify-start gap-y-1.5 p-3 text-center ${className && className}`}
+        >
             <h3 className="font-title text-lg leading-tight font-semibold">
                 {title}
             </h3>
--- resources/js/Pages/Welcome/UseCase.tsx
+++ resources/js/Pages/Welcome/UseCase.tsx
@@ -16,34 +16,38 @@ const UseCase = () => {
                 </p>
             </div>
             <div className="flex flex-wrap items-center justify-center gap-y-2">
-                <div className="flex w-1/2 items-center justify-center">
+                <div className="flex w-full items-center justify-start">
                     <TitleText
                         title={'Streamline Your Buylist'}
                         text={
                             'See what you actually need to buy — and what you already own across decks.'
                         }
+                        className="items-start !text-left"
                     />
                 </div>
-                <div className="flex w-1/2 items-center justify-center">
+                <div className="flex w-full items-center justify-start">
                     <TitleText
                         title={'Brew Variants with Ease'}
                         text={
                             'Build multiple takes on the same commander or archetype without duplicating effort — or cards.'
                         }
+                        className="items-start !text-left"
                     />
                 </div>
-                <div className="flex w-1/2 items-center justify-center">
+                <div className="flex w-full items-center justify-start">
                     <TitleText
                         title={'See What’s Recyclable'}
                         text={
                             'Find cards that are underused or used in just one deck — and reassign them to new builds.'
                         }
+                        className="items-start !text-left"
                     />
                 </div>
-                <div className="flex w-1/2 items-center justify-center">
+                <div className="flex w-full items-center justify-start">
                     <TitleText
                         title={'Check Your Power Level'}
                         text={'Get a Rule-0 snapshot of your deck.'}
+                        className="items-start !text-left"
                     />
                 </div>
             </div>
