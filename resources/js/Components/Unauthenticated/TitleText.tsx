ffbd8e4873afe880bed7fe60bb1deaa10e5d7ef2
Nick Zou <z.nick00@gmail.com>
2025-06-13 23:27:23 -0400

    update TitleText component

--- resources/js/Components/Unauthenticated/TitleText.tsx
+++ resources/js/Components/Unauthenticated/TitleText.tsx
@@ -6,12 +6,12 @@ type Props = {
 const TitleText = ({ title, text, className }: Props) => {
     return (
         <div
-            className={`flex flex-col items-center justify-start gap-y-1.5 p-3 text-center ${className && className}`}
+            className={`flex grow flex-col items-center justify-start gap-y-1.5 border-b border-solid border-rose-500 py-3 text-center ${className && className}`}
         >
             <h3 className="font-title text-lg leading-tight font-semibold">
                 {title}
             </h3>
-            <p className="text-sm">{text}</p>
+            <p className="text-sm leading-6">{text}</p>
         </div>
     );
 };
