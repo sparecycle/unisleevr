ed29b9d2aa5acf7b987c35f6ded337217e56ee3d
Nick Zou <z.nick00@gmail.com>
2025-06-18 22:15:09 -0400

    add background

--- resources/js/Components/Unauthenticated/Section.tsx
+++ resources/js/Components/Unauthenticated/Section.tsx
@@ -3,14 +3,19 @@ import { ReactNode } from 'react';
 type Props = {
     className?: string;
     children: ReactNode;
+    useBackground?: boolean;
+    background?: ReactNode;
 };
 
-const Section = ({ className, children }: Props) => {
+const Section = ({ className, children, useBackground, background }: Props) => {
     return (
-        <section className={`py-16 text-white lg:py-24 xl:py-24 ${className}`}>
+        <section
+            className={`relative py-16 text-white lg:py-24 xl:py-24 ${className}`}
+        >
             <div className="container mx-auto flex flex-col gap-2 px-4">
                 {children}
             </div>
+            {useBackground && <>background</>}
         </section>
     );
 };
