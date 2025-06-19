6ceedcb9446eb3129953b350a14c9b8a3976cf48
Nick Zou <z.nick00@gmail.com>
2025-06-18 21:53:49 -0400

    add markup

--- resources/js/Components/Picture.tsx
+++ resources/js/Components/Picture.tsx
@@ -47,7 +47,37 @@ const Picture = ({
     const baseName = getBaseName(src);
     const extension = getExtension(src);
 
-    return <picture></picture>;
+    return (
+        <picture className={className}>
+            <source
+                media={`(min-width: ${breakpoints?.large}px)`}
+                srcSet={`${baseName}-${sizes?.ultra}${extension}`}
+            />
+            <source
+                media={`(min-width: ${breakpoints?.desktop}px)`}
+                srcSet={`${baseName}-${sizes?.large}${extension}`}
+            />
+            <source
+                media={`(min-width: ${breakpoints?.small}px)`}
+                srcSet={`${baseName}-${sizes?.desktop}${extension}`}
+            />
+            <source
+                media={`(min-width: ${breakpoints?.tablet}px)`}
+                srcSet={`${baseName}-${sizes?.small}${extension}`}
+            />
+            <source
+                media={`(min-width: ${breakpoints?.mobile}px)`}
+                srcSet={`${baseName}-${sizes?.tablet}${extension}`}
+            />
+            <img
+                src={`${baseName}-${sizes?.mobile}${extension}`}
+                alt={alt}
+                className="h-auto w-full"
+                loading={loading}
+                fetchPriority={fetchPriority}
+            />
+        </picture>
+    );
 };
 
 export default Picture;
