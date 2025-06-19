99b6ed13c68e6f912258ed6ef382537128ef9024
Nick Zou <z.nick00@gmail.com>
2025-06-18 20:22:48 -0400

    update section padding on desktop

--- resources/js/Components/Unauthenticated/Section.tsx
+++ resources/js/Components/Unauthenticated/Section.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const Section = ({ className, children }: Props) => {
     return (
-        <section className={`py-16 text-white lg:py-24 ${className}`}>
+        <section className={`py-16 text-white lg:py-24 xl:py-24 ${className}`}>
             <div className="container mx-auto flex flex-col gap-2 px-4">
                 {children}
             </div>
