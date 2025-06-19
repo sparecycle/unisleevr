f0c29b32d35b245569d7fbf3096b337fda82de1c
Nick Zou <z.nick00@gmail.com>
2025-06-18 21:57:23 -0400

    update heading group component

--- resources/js/Pages/Welcome/Problem.tsx
+++ resources/js/Pages/Welcome/Problem.tsx
@@ -5,15 +5,17 @@ import SectionTitle from '@/Components/Unauthenticated/SectionTitle';
 const Problem = () => {
     return (
         <Section className="min-h-1/2">
-            <SectionTitle eyebrow="Never buy duplicates again.">
-                Where's My Rhystic Study?
-            </SectionTitle>
-            <Paragraph>
-                Keeping track of shared staples in Commander is a headache.
-                You’re never sure which deck your Rhystic Study is in, you’ve
-                bought extras just to stay legal, and proxies are piling up
-                fast. It’s chaotic, expensive, and all too common.
-            </Paragraph>
+            <div className="flex flex-col items-start gap-y-3 xl:w-3/5">
+                <SectionTitle eyebrow="Never buy duplicates again.">
+                    Where's My Rhystic Study?
+                </SectionTitle>
+                <Paragraph>
+                    Keeping track of shared staples in Commander is a headache.
+                    You’re never sure which deck your Rhystic Study is in,
+                    you’ve bought extras just to stay legal, and proxies are
+                    piling up fast. It’s chaotic, expensive, and all too common.
+                </Paragraph>
+            </div>
         </Section>
     );
 };
