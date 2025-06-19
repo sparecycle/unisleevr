2f1931bd9a6449baa5c46dc8284907d73226a883
dazu1990 <adrian.davilazuniga@gmail.com>
2025-06-18 21:51:12 -0400

    added horizontal stagger to cube bg

--- resources/js/Components/CubeBg.tsx
+++ resources/js/Components/CubeBg.tsx
@@ -16,19 +16,31 @@ export const CubeBg = () => {
     const cubesToRender = isMediumOrLarger ? Math.ceil(screenWidth / 10) : 30;
     const columns = isMediumOrLarger ? 14 : 5;
 
+    const rowOffsets = Array.from(
+        { length: Math.ceil(cubesToRender / columns) },
+        () => Math.floor(Math.random() * 100) - 50, // random offset between -30px and 30px
+    );
+
     return (
-        <div className="cube-bg-grid -mt-[30px] -ml-[32px] grid h-full w-full grid-cols-5 grid-rows-6 gap-x-25 gap-y-30 md:-mt-0 md:grid-cols-8 md:grid-rows-8 md:gap-x-20 md:gap-y-24">
+        <div className="cube-bg-grid -mt-[30px] -ml-[32px] grid h-full w-full grid-cols-5 grid-rows-6 gap-x-25 gap-y-30 md:-mt-0 md:grid-cols-10 md:grid-rows-8 md:gap-x-20 md:gap-y-24">
             {Array.from({ length: cubesToRender }).map((_, i) => {
-                // Generate random starting rotations for each cube
+                const row = Math.floor(i / columns);
+                const col = i % columns;
+                const shouldStagger = (row + col) % 2 === 1;
                 const initialX = Math.floor(Math.random() * 360);
                 const initialY = Math.floor(Math.random() * 360);
+                const rowOffset = rowOffsets[row];
+
                 return (
                     <div
                         key={i}
                         className={
                             'cube-3d mx-auto my-auto' +
-                            ((i % columns) % 2 === 1 ? ' cube-3d-stagger' : '')
+                            (shouldStagger ? ' cube-3d-stagger' : '')
                         }
+                        style={{
+                            transform: `translateX(${rowOffset}px)`, // add to any existing transform if needed
+                        }}
                     >
                         <div
                             className="cube-3d-inner"
