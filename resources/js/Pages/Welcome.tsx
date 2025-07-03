03e2eacd450ced637b0447d589a418142cf99cc8
dazu1990 <adrian.davilazuniga@gmail.com>
2025-07-02 22:09:11 -0400

    fixed unauthicated nav

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -1,6 +1,7 @@
 import { PageProps } from '@/types';
 import { Head } from '@inertiajs/react';
 // import BottomCta from './Welcome/BottomCta';
+import { usePage } from '@inertiajs/react';
 import Header from '../Components/Header';
 import Hero from './Welcome/Hero';
 import Problem from './Welcome/Problem';
@@ -15,13 +16,15 @@ export default function Welcome({
     unisleevrVersion: string;
     phpVersion: string;
 }>) {
+    const user = usePage().props.auth.user;
+
     return (
         <>
             <Head>
                 <title>Welcome</title>
             </Head>
             <div className="relative flex min-h-screen w-full flex-col overflow-x-clip bg-zinc-900">
-                <Header auth={auth} />
+                <Header auth={user} />
                 <main>
                     <Hero />
                     <Problem />
