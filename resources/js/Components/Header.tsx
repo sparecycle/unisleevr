e834554d7e715fe09a8d18c97c372ced19fd1487
Nick Zou <z.nick00@gmail.com>
2025-07-02 22:25:31 -0400

    fix m to p

--- resources/js/Components/Header.tsx
+++ resources/js/Components/Header.tsx
@@ -12,48 +12,50 @@ const Header = ({ auth }: Props) => {
     const [open, setOpen] = useState<boolean>(false);
     return (
         <>
-            <div className="sticky z-10 m-3 mt-0 flex h-12 items-center justify-between rounded-md bg-black bg-zinc-900 p-3 text-white">
-                <nav className="flex h-full w-auto">
-                    <Link
-                        href="/"
-                        className="h-full w-auto [&_polyline]:fill-none [&_polyline]:stroke-zinc-200"
-                    >
-                        <ApplicationLogo className="block h-full w-auto fill-current" />
-                    </Link>
-                    <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                        {auth ? (
-                            <>
-                                <NavLink
-                                    href={route('decks.index')}
-                                    active={route().current('decks.index')}
-                                >
-                                    Decks
-                                </NavLink>
-                                <NavLink
-                                    href={route('cards.index')}
-                                    active={route().current('cards.index')}
-                                >
-                                    Card Pool
-                                </NavLink>
-                            </>
-                        ) : (
-                            <>
-                                <NavLink
-                                    href={route('login')}
-                                    active={route().current('login')}
-                                >
-                                    Login
-                                </NavLink>
-                                <NavLink
-                                    href={route('register')}
-                                    active={route().current('register')}
-                                >
-                                    Register
-                                </NavLink>
-                            </>
-                        )}
-                    </div>
-                </nav>
+            <div className="sticky z-10 p-3">
+                <div className="flex h-12 items-center justify-between rounded-md bg-zinc-900 p-3 text-white">
+                    <nav className="flex h-full w-auto">
+                        <Link
+                            href="/"
+                            className="h-full w-auto [&_polyline]:fill-none [&_polyline]:stroke-zinc-200"
+                        >
+                            <ApplicationLogo className="block h-full w-auto fill-current" />
+                        </Link>
+                        <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                            {auth ? (
+                                <>
+                                    <NavLink
+                                        href={route('decks.index')}
+                                        active={route().current('decks.index')}
+                                    >
+                                        Decks
+                                    </NavLink>
+                                    <NavLink
+                                        href={route('cards.index')}
+                                        active={route().current('cards.index')}
+                                    >
+                                        Card Pool
+                                    </NavLink>
+                                </>
+                            ) : (
+                                <>
+                                    <NavLink
+                                        href={route('login')}
+                                        active={route().current('login')}
+                                    >
+                                        Login
+                                    </NavLink>
+                                    <NavLink
+                                        href={route('register')}
+                                        active={route().current('register')}
+                                    >
+                                        Register
+                                    </NavLink>
+                                </>
+                            )}
+                        </div>
+                    </nav>
+                </div>
 
                 <div className="relative h-12 w-12 grow-0 sm:hidden">
                     <Hamburger size={20} toggled={open} toggle={setOpen} />
